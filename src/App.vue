332aca47d696eac6d4ae73558a68bf5a5f6bf699
Akeeb Ismail <damiz.akeeb@gmail.com>
2018-03-12 20:43:33 +0100

    add bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
+
 
 <script>
-export default {
-  name: 'App'
-}
+    import BootStrap from 'bootstrap-vue'
+    import 'bootstrap/dist/css/bootstrap.css';
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
